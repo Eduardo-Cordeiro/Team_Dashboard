38fd708a579cf7339d59185aa015c91561853c6c
Eduardo Cordeiro <56442919+Eduardo-Cordeiro@users.noreply.github.com>
2024-06-30 20:41:40 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,6 +3,7 @@ import requests
 from io import StringIO
 import streamlit as st
 import streamlit.components.v1 as components
+import plotly.express as px
 
 url1 = 'https://raw.githubusercontent.com/Eduardo-Cordeiro/Team_Dashboard/main/Jira_Data.csv'
 response1 = requests.get(url1)
@@ -12,4 +13,18 @@ jira = pd.read_csv(StringIO(csv_content1), delimiter=',')
 
 jira = pd.read_csv('Jira_Data.csv')
 
-print(jira['Chave do projeto'].value_counts())
+st.title("Jira")
+
+# Dropdown to select city
+projec = st.selectbox("Select a project", options=[jira['Chave do projeto'].unique()])
+
+# Filter DataFrame based on city
+filtered_df = jira[jira['Chave do projetoy'] == projec]
+
+# Plotting the bar chart
+fig = px.bar(filtered_df, x='Fruit', y='Amount', color='Fruit', barmode='group')
+
+# Display the bar chart
+st.plotly_chart(fig)
+
+
