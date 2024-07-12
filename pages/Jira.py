4fd98fae94d2ac513f2eff92c5a4bb93859cdc1e
Eduardo Cordeiro <56442919+Eduardo-Cordeiro@users.noreply.github.com>
2024-07-11 21:27:38 -0300

    Update Jira.py

--- pages/Jira.py
+++ pages/Jira.py
@@ -11,52 +11,42 @@ response1.raise_for_status()
 csv_content1 = response1.text
 jira = pd.read_csv(StringIO(csv_content1), delimiter=',')
 jira["Chave do projeto"] = jira["Chave do projeto"].apply(lambda x: "PHON" if x == "PHONE" else x) 
+jira["Criado"] = pd.to_datetime(jira["Criado"])
 
+
+st.title("Jira Dashboard")
+
+# Construindo Widgets iniciais
+min_date = jira["Criado"].min().date()
+max_date = jira["Criado"].max().date()
+
+col1, col2, col3, col4 = st.columns([1,1,1,1])
+col1, col2 = st.columns([1,1])
+with col1:
+    proj_selected = st.multiselect('Projeto', ["PLAT",'ROUT','SITE',"NET",'BASE','CONN','DATA','PHON'],default=["PLAT",'ROUT','SITE',"NET",'BASE','CONN','DATA','PHON'])
+    start = st.date_input('Data Inicial',min_value=min_date, max_value=max_date, value=min_date)
+with col2:
+    satis = st.multiselect('Prioridade', ['Lowest','Low','Medium','High','Highest'],default=['Lowest','Low','Medium','High','Highest'])
+    end = st.date_input('Data Final',min_value=min_date, max_value=max_date, value=max_date)
+
+jira = jira[(jira["Chave do projeto"].isin(proj_selected)) & (jira["Prioridade"].isin(satis)) & (jira["Criado"] > pd.Timestamp(start)) & (jira["Criado"] < pd.Timestamp(end)) ]
 # Create the initial bar chart
 qtd_jira = pd.DataFrame()
 qtd_jira['Projeto'] = jira['Chave do projeto'].value_counts().index
 qtd_jira['Quantidade'] = jira['Chave do projeto'].value_counts().values
+
 fig = px.bar(qtd_jira, x='Projeto', y='Quantidade')
 fig.update_traces(marker_color='lightskyblue', selector=dict(type='bar'))
 
-col1, col2, col3, col4 = st.columns([1,1,1,1])
-
-with col1:
-    st.multiselect('Projeto', [1,2,3])
-with col2:
-    st.multiselect('Satisfaçao', ['Good','Bad'])
-with col3:
-    st.date_input('Data Inicial')
-with col4:
-    st.date_input('Data Final')
-
-
-# Initialize session state for each project's table visibility
-for project in qtd_jira['Projeto']:
-    if f'show_{project}' not in st.session_state:
-        st.session_state[f'show_{project}'] = False
-        st.session_state[f'show_{"PLAT"}'] = True
-
-# Create a list of options for the dropdown
-options = qtd_jira['Projeto']
-
-# Main categories per project
-
-# Create a list of options for the dropdown
-options2 = qtd_jira["Projeto"]
-
-st.title("Jira Dashboard")
 st.markdown("### Quantidade de chamados por Projeto")
 st.plotly_chart(fig)
+
 st.markdown("### Dataframes por Projeto")
 st.markdown("Selecione o Projeto e visualize as pricipais informações sobre as issues.")
-selected_option = st.selectbox('Projetos:', options)
-st.dataframe(jira[jira["Chave do projeto"] == selected_option][["Chave do item","Solicitante","Prioridade","Criado"]],hide_index=True,use_container_width=True)
-
-selected_option2 = st.selectbox('Projetos', options2)
+st.dataframe(jira,hide_index=True)
 category = pd.DataFrame()
-category["Categoria"] = jira[jira["Chave do projeto"] == selected_option2]["Campo personalizado (Categoria)"].value_counts().index
-category["Quantidade"] = jira[jira["Chave do projeto"] == selected_option2]["Campo personalizado (Categoria)"].value_counts().values
+category["Categoria"] = jira["Campo personalizado (Categoria)"].value_counts().index
+category["Quantidade"] = jira["Campo personalizado (Categoria)"].value_counts().values
 fig2 = px.bar(category.head(15), x='Categoria', y='Quantidade')
 
 fig2.update_traces(marker_color='lightskyblue', selector=dict(type='bar'))
